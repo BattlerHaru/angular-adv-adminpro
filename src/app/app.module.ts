18387272bdb98f15b28f0f9565458f928f016d2e
V-Haru <kronk.sdance@gmail.com>
2021-02-02 18:01:55 -0600

    Diseño y rutas base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    LoginComponent,
+    RegisterComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NopagefoundComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

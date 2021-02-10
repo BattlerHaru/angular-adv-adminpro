20f17fc21aef6b35df5c31ea1bfa28f842e2f42c
V-Haru <kronk.sdance@gmail.com>
2021-02-09 21:27:31 -0600

    se agrego funcionabilidad al progress

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 // Modulos
-// import { AppRoutingModule } from '../app-routing.module';
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+
 
 @NgModule({
   declarations: [
@@ -26,8 +28,10 @@ import { PagesComponent } from './pages.component';
   ],
   imports: [
     CommonModule,
-    SharedModule,
     RouterModule,
+    FormsModule,
+    SharedModule,
+    ComponentsModule,
   ]
 })
 export class PagesModule { }

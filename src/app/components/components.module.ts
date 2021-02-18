863dbcdacb1fbc4b0e3331539c129a7c155f1454
V-Haru <kronk.sdance@gmail.com>
2021-02-18 13:23:32 -0600

    fin seccion 6  - se modifico el progress para ser un comp y se tomo como base para crear la grafica

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ChartsModule } from 'ng2-charts';
+
 import { IncrementadorComponent } from './incrementador/incrementador.component';
+import { DoughnutComponent } from './doughnut/doughnut.component';
 
 
 
 @NgModule({
-  declarations: [IncrementadorComponent],
+  declarations: [
+    IncrementadorComponent, DoughnutComponent
+  ],
   exports: [
     IncrementadorComponent,
+    DoughnutComponent,
   ], imports: [
     CommonModule,
+    FormsModule,
+    ChartsModule,
   ]
 })
 export class ComponentsModule { }
--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class IncrementadorComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit() { this.btnClass = `btn ${this.btnClass}` }
 
-  ngOnInit(): void {
+  // Recibe
+  @Input('valorEntrada') progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
+
+  // Envia
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
+
+  notRange: boolean = false;
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      return this.progreso = 100
+
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
+      return this.progreso = 0
+    }
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  }
+
+
+  onChange(nuevoValor: number) {
+    console.log(nuevoValor);
+    if (nuevoValor > 100) {
+      this.progreso = 100;
+    }
+    else if (nuevoValor < 0) {
+      this.progreso = 0;
+    }
+    else {
+      this.progreso = nuevoValor;
+      this.valorSalida.emit(this.progreso);
+    }
   }
 
 }
